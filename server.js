e2efb973cce0bc7948b6be099178b4c6c734d26a
Shishir <shishir16subedi@gmail.com>
2024-06-21 22:11:17 +0545

    initial

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 3000 || process.env.PORT;
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+
+const server = app.listen(port, () => {
+    console.log('Server is running on port: ' + port);
+
+});
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: ["http://localhost:8080"]
+    }
+});
+
+io.on('connection', socket => {
+    socket.on('user-connected', (name) => {
+        socket.broadcast.emit('user-joined-info',name);
+    });
+    socket.on('send-message', (msg, who) => {
+        socket.broadcast.emit('receive-message', msg, who);
+    });
+    socket.on('user-disconnected', (who) => {
+        socket.broadcast.emit('user-exit-info', who);
+    });
+});
+
+
